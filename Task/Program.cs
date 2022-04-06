369722abb04ce7c0a4840fa723ca1d57f76714d2
Stanislav <redkorona@yandex.ru>
2022-04-06 03:14:43 +0300

    add Task

---
+++ Task/Program.cs
@@ -0,0 +1,19 @@
+﻿void PrintArray(string[] arr) // печать для проверки задания
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + "   ");
+    }
+}
+string[] array = { "hello", "2", "world", ":-)" };
+string[] newArray = new string[array.Length - 1];
+int n = 0;
+for (int step = 0; step < array.Length; step++)
+{
+    if (array[step].Length <= 3)
+    {
+        newArray[n] = array[step];
+        n++;
+    }
+}
+PrintArray(newArray);
